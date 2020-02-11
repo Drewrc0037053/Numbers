1a3d53376b04f45b87c4f7e2b8267f89f808a02a
Drew Doran <rc0037053@student.riversidecollege.ac.uk>
2020-02-11 12:27:38 +0000

    Added the three unexpected inputs

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -36,6 +36,38 @@ namespace Numbers
             richTextBox1.AppendText(c.ToString("N") + "\n");
             richTextBox1.AppendText(c.ToString("0.000000000000000E0") + "\n");
             richTextBox1.AppendText(c.ToString("0.00E0") + "\n");
+
+
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an integer");
+            }
+
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + " is not an integer");
+                int i2 = 0;
+            }
+
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
